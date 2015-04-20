eaac972a534cb756bf58205585ddc994ceef2837
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-04-20 12:21:56 +0200

    set dependencies, add dummy route

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,20 +5,48 @@ export interface IRegister {
 
 export default
 class Locationpool {
+    private joi:any;
+    private boom:any;
+    private db:any;
+
     constructor() {
         this.register.attributes = {
             name: 'backend-locationpool',
             version: '0.1.0'
         };
+        this.joi = require('joi');
+        this.boom = require('Boom');
+
+        // TODO: create validation scheme with joi
     }
 
     register:IRegister = (server, options, next) => {
         server.bind(this);
+
+        // set dependency to the database plugin
+        server.dependency('backend-database', (server, next) => {
+            this.db = server.plugins['backend-database'];
+            next();
+        });
+
         this._register(server, options);
         next();
     };
 
     private _register(server, options) {
+        //register all routes
+        server.route({
+            method: 'GET',
+            path: '/user/{userID}/locations',
+            config: {
+                handler: (request, reply) => {
+                    // config the handler
+                },
+                description: '', // TODO
+                tags: ['', ''] //TODO
+            }
+        });
+
         // Register
         return 'register';
     }
